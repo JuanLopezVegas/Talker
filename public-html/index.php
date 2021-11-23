9cc7ca95c9739026af7e408159a2a0b95f1acebb
Juan Lopez <juanlopezvegas@protonmail.com>
2021-11-23 08:22:16 +0100

    Bootstrap start template

--- public-html/index.php
+++ public-html/index.php
@@ -1,152 +1,54 @@
-<?php
-  $website = 'https://duckduckgo.com';
- ?>
-
-
 <!DOCTYPE html>
-<head>
- <title>Hello World!</title>
-</head>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>TALKER</title>
 
-<body>
- <h1>Hello World!</h1>
- <a=href="<?php echo $website; ?>"> <?php echo "Hola mother fucker: $website"; ?>   </a>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  </head>
+  <body>
+          <div class="container">
+          	<div class="row justify-content-md-center">
+          		<div class="col-12 col-md-auto"><h1>TALKER | SIGN UP</h1></div>
+          	</div>
 
- <p>
-   <?php
-    $juan=['1','12356','arst'];
-    $countries = array('Finland', 'Fuck you', 'pussy' );
-    print_r($countries);
-    print_r($juan);
-    $countries[]='Italia picolo bambino';
-    print_r($countries);
-    ?>
- </p>
+          	<hr><br>
 
- <p>
-   <?php
-     echo $countries[1];
-    ?>
- </p>
+          	<div class="row">
+          		<div class="col-6">
+          			<form>
+          				<div class="form-group">
+          					<label for="formSignUpEmail">Email address</label>
+          					<input type="email" class="form-control" id="formSignUpEmail" placeholder="Enter your email address">
+          				</div>
+          				<div class="form-group">
+          					<label for="formSignUpPassword">Password</label>
+          					<input type="password" class="form-control" id="formSignUpPassword" placeholder="Enter your password">
 
- <p>
-   <?php
-     echo count ($countries);
-    ?>
- </p>
+          					<input type="password" class="form-control mt-4" id="formSignUpPasswordConf" placeholder="Confirm your password">
+          				</div>
+          				<button type="submit" class="btn btn-primary">Submit</button>
+          			</form>
+          		</div>
 
- <p>
-   <?php
-     $age = array(
-       'juan' => 25,
-        'hernesto' => 35,
-      'mickey pluto' => 1234,
-    );
-    print_r($age);
-    ?>
- </p>
-
- <p>
-   <?php
-     echo  $age['hernesto'];
-    ?>
- </p>
-
- <script>
-   var movies=["shawshak","Inception", "MJ Demarco", "Jim Kwik"];
-   for (i in movies){
-      console.log("The current value of the array is " + movies[i]);
-   }
- </script>
-
- <<?php
-   $movies = ["shawshak", "Inception", "MJ Demarco", "Jim Kwik"];
-   foreach ($movies as $i) {
-     echo "The current value of the mother fucker is $i<br>";
-   }
-  ?>
-
-<p>
-  <?php
-    class carBluePrint {
-      // here goes properties and method
-      //public $color = "Victoria Secret Model";
-      //public $make = "Heidi Klum";
-
-      //constructor
-      public function __construct($newColor, $newMake){
-        $this->color = $newColor;
-        $this->make = $newMake;
-      }
+          		<div class="col-6">
+          			<p>Hello and welcome to Talker! We are very happy that you want to join our great community!</p>
+          			<p>Please, enter your email and password. Your must have access to your email because we will send
+                    a confirmation code to that address. Your password must be between 8 and 16 characters long, with at
+                    least one uppercase and one lowercase character, one number and one special character (@, *, $ or #).</p>
+          			<p>We hope you'll enjoy Talker!</p>
+          		</div>
+          	</div>
+          </div>
 
 
-      // setter method
-      public function setColor($newColor){
-        $this->color = $newColor;
-      }
-
-      //getter method
-      public function getColor(){
-          return "<br>New color is : " . $this->color . "<br>";
-      }
-    }
-
-    $firstRealCar = new carBluePrint('arbre','Space X');
-
-    var_dump ($firstRealCar);
-    echo $firstRealCar->color;
-
-
-    echo $firstRealCar->getColor();
-
-    $secondRealCar = new carBluePrint('ciel','Navidad');
-
-
-    echo $secondRealCar->getColor();
-    var_dump($secondRealCar);
-
-   ?>
-</p>
-
-<p>
-  <<?php
-    class sportCarBluePrint extends carBluePrint {
-      public function __construct($newColor, $newMake, $newSpoiler){
-        parent::__construct($newColor, $newMake);
-        $this->spoiler = $newSpoiler;
-      }
-
-      public function activateSpoiler(){
-        return "<br><strong>SPOILER ACTIVE!</strong></br>";
-      }
-    }
-
-    $firstSportCar = new sportCarBluePrint('Lelouch', 'CC', 'Kyra');
-    $firstSportCar->setColor('Daito');
-    var_dump($firstSportCar);
-    $firstSportCar->activateSpoiler();
-   ?>
-</p>
-
-<p>
-  <<?php
-    function divideOneByNumber($number){
-      if ($number==0){
-        throw new exception ("Division by ziro is not allowed.");
-      }
-      return 1/$number;
-    }
-
-    try{
-        echo "The mother fucker pussy is" .  divideOneByNumber(0);
-    }
-
-    catch(Exception $e) {
-      echo "Message;" . $e->getMessage();
-    }
-   ?>
-</p>
-
-
-
-</body>
+    <!-- Optional Javascript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
