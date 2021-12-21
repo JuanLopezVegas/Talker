eb9391d91861203d926aef6c68271804e45f320a
Juan Lopez <juanlopezvegas@protonmail.com>
2021-12-21 07:09:11 +0100

    Creating the messagind module

--- public-html/gate.php
+++ public-html/gate.php
@@ -58,6 +58,10 @@ session_start(); require('system.ctrl.php');
 				<li class="nav-item <?php if ($_GET['module']=='') { echo 'active';} ?>">
 					<a class="nav-link" href="gate.php">Home <?php if ($_GET['module']=='') { ?> <span class="sr-only">(current)</span><?php } ?> </a>
 				</li>
+				<li class="nav-item <?php if ($_GET['module']=='messaging') { echo 'active';} ?>">
+					<a class="nav-link" href="gate.php?module=messaging">Messaging <?php if ($_GET['module']=='messaging') { ?> <span class="sr-only">(current)</span><?php } ?> </a>
+				</li>
+				<li class="nav-item">
 				<li class="nav-item <?php if ($_GET['module']=='settings') { echo 'active';} ?>">
 					<a class="nav-link" href="gate.php?module=settings">Settings <?php if ($_GET['module']=='settings') { ?> <span class="sr-only">(current)</span><?php } ?> </a>
 				</li>
@@ -65,6 +69,7 @@ session_start(); require('system.ctrl.php');
 					<a class="nav-link" href="logout.ctrl.php">Logout</a>
 				</li>
 			</ul>
+      <a class="nav-link-user" href="#"><?php echo $dbUserRow["user_email"]; ?></a>
 		</div>
 	</nav>
 
@@ -87,6 +92,10 @@ session_start(); require('system.ctrl.php');
     switch ($_GET["module"]) {
     	case "settings":
     	include('settings.php');
+    	break;
+
+    	case "messaging":
+    	include('messaging.php');
     	break;
 
     	default:
---
+++ public-html/messaging.php
@@ -0,0 +1,23 @@
+<?php
+	$db_data = array($_SESSION["uid"]);
+	$dbUserRow = phpFetchDB('SELECT * FROM user WHERE user_id = ?', $db_data);
+	$db_data = "";
+
+
+if ($dbUserRow["user_verified"] == 0) {{}
+  ?>
+
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-3">Access denied!</h1>
+      <p class="lead">You need to verify your email address before you can use this feature.</p>
+    </div>
+  </div>
+
+<?php
+
+} else {
+  echo "display the form";
+}
+
+?>
