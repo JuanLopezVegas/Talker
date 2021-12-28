695ae33aaf5f59687c89f0b8279ed42a4795e331
Juan Lopez <juanlopezvegas@protonmail.com>
2021-12-28 09:08:03 +0100

    Deleting groups

--- public-html/system.ctrl.php
+++ public-html/system.ctrl.php
@@ -44,11 +44,16 @@ function phpShowSystemFeedback($feedback_id) {
 		$feedback_text="Group name has been changed   successfully :)";
 		break;
 
+		case "413":
+		$feedback_type="success";
+		$feedback_text="Group has been deleted   successfully :)";
+		break;
+
 		case "511":
 		$feedback_type="success";
 		$feedback_text="Post has been   successfully sent:)";
 		break;
-  
+
 		case "512":
 		$feedback_type="success";
 		$feedback_text="Post has been   successfully updated:)";
